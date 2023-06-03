058d90794536e3fc7c6e7059aebf2534c7bae380
EkaterinaMelnikova <katkimo@gmail.com>
2023-06-03 15:46:25 +0300

    Добавлен конструктор в класс Paint

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,12 +4,8 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        Point p1= new Point();
-        Point p2= new Point();
-        p2.x = 5;
-        p1.x = 2;
-        p2.y = 6;
-        p1.y = 1;
+        Point p1= new Point(2,1);
+        Point p2= new Point(5,6);
 
         System.out.println("Расстояние между точками=" + distance(p2, p1 ));
     }
