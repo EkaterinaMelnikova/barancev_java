0318dff3844723b0d2c85ca2b122626b9c1e1c2e
EkaterinaMelnikova <katkimo@gmail.com>
2023-06-03 14:49:11 +0300

    Версия с классом  Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,16 +4,19 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        double x2 = 5;
-        double x1 = 2;
-        double y2 = 6;
-        double y1 = 1;
-        System.out.println("Расстояние между Х и Y=" + distance(x2, x1, y2, y1));
+        Point p1= new Point();
+        Point p2= new Point();
+        p2.x = 5;
+        p1.x = 2;
+        p2.y = 6;
+        p1.y = 1;
+
+        System.out.println("Расстояние между точками=" + distance(p2, p1 ));
     }
 
-    public static double distance(double x1, double x2, double y1, double y2) {
+    public static double distance(Point p1,Point p2 ) {
 
-        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
     }
 }
 
