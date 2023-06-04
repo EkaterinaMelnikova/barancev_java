e76bb5e4fb5a9725abaafb7ffe6c8c4c1404ea83
EkaterinaMelnikova <katkimo@gmail.com>
2023-06-04 12:03:20 +0300

    Изменен Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,8 +4,8 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        Point p1= new Point(2, 1);
-        Point p2= new Point(5,6);
+        Point p1= new Point(5, 10);
+        Point p2= new Point(5,10);
 
 
 
---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+    @Test
+    public void testDistance (){
+     Point p1= new Point (5, 10)   ;
+     Point p2= new Point (5, 10)   ;
+
+     assert distance == 0;
+    }
+}
