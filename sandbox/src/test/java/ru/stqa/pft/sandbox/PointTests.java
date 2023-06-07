c28d15f192dfe2b885eb012f851713ee3d7ec1f9
EkaterinaMelnikova <katkimo@gmail.com>
2023-06-07 08:43:28 +0300

    Несколько тестов для отрезка

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -6,10 +6,38 @@ import org.testng.annotations.Test;
 public class PointTests {
 
     @Test
-    public void testDistance (){    /*название тестового метода*/
-     Point p1= new Point (5, 10)   ; /*создаем новую точку*/
+    public void testDistance1 (){
+     Point p1= new Point (5, 10)   ;
      Point p2= new Point (5, 10)   ;
 
         Assert.assertEquals( p2.distance(p1) ,0.0);
+    }
+    @Test
+    public void testDistance2 (){
+        Point p1= new Point (5, 10)   ;
+        Point p2= new Point (5, 10)   ;
+
+        Assert.assertEquals( p2.distance(p1) ,0.1);
+    }
+    @Test
+    public void testDistance3 (){
+        Point p1= new Point (0.5, 5.5)   ;
+        Point p2= new Point (5, 10)   ;
+
+        Assert.assertEquals( p2.distance(p1) ,6.363961030678928);
+    }
+    @Test
+    public void testDistance4 (){
+        Point p1= new Point (0.5, 5.5)   ;
+        Point p2= new Point (5, 10)   ;
+
+        Assert.assertEquals( p2.distance(p1) ,0.1);
+    }
+    @Test
+    public void testDistance5 (){
+        Point p1= new Point (5, 10)   ;
+        Point p2= new Point (-5, -10)   ;
+
+        Assert.assertEquals( p2.distance(p1) ,22.360679774997898);
     }
 }
