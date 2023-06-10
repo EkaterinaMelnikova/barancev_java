06d78edbc00c99e6ddeadb030d35abbde60d91d6
EkaterinaMelnikova <katkimo@gmail.com>
2023-06-10 16:38:51 +0300

    Логин в @BeforeMethod

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/CreateGroup4.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/CreateGroup4.java
@@ -15,10 +15,6 @@ public class CreateGroup4 {
     wd = new FirefoxDriver();
 
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testCreateGroup4() throws Exception {
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -27,6 +23,11 @@ public class CreateGroup4 {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testCreateGroup4() throws Exception {
+
     wd.findElement(By.xpath("//body")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
