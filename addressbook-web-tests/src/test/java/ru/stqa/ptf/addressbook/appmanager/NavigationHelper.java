1eb6953a8efc2ae1ae44fedd36fe5c3f6df47ac4
EkaterinaMelnikova <katkimo@gmail.com>
2023-06-13 21:03:29 +0300

    Изменен  NavigationHelper

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/NavigationHelper.java
@@ -3,15 +3,15 @@ package ru.stqa.ptf.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper {
-    private WebDriver wd;
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void gotoGroupPage() {
-        wd.findElement(By.xpath("//body")).click();
+
+        click(By.xpath("//body"));
     }
 
 
