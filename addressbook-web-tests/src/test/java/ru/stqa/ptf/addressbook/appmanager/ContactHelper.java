7ae71c8ec315aab79e3839c0e47919d20cbb10bd
EkaterinaMelnikova <katkimo@gmail.com>
2023-06-14 20:06:31 +0300

    Доработка PointTests

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ContactHelper.java
@@ -5,13 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import ru.stqa.ptf.addressbook.model.ContactData;
 
-public class ContactHelper {
-    private WebDriver wd;
+public class ContactHelper extends HelperBase{
+
 
     public ContactHelper(WebDriver wd) {
 
-
-        this.wd = wd;
+        super(wd);
     }
 
     public void logout() {
@@ -28,93 +27,54 @@ public class ContactHelper {
     }
 
     public void fillContactForm(ContactData contactData) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("middlename")).click();
-        wd.findElement(By.name("middlename")).click();
-        wd.findElement(By.name("middlename")).clear();
-        wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-        wd.findElement(By.name("nickname")).click();
-        wd.findElement(By.name("nickname")).clear();
-        wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
-        wd.findElement(By.name("title")).click();
-        wd.findElement(By.name("title")).clear();
-        wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
+        type(By.name("firstname"),contactData.getFirstname());
+        type(By.name("middlename"),contactData.getMiddlename());
+        type(By.name("lastname"),contactData.getLastname());
+        type(By.name("nickname"),contactData.getNickname());
+        type(By.name("title"),contactData.getTitle());
+
         wd.findElement(By.name("theform")).click();
-        wd.findElement(By.name("company")).click();
-        wd.findElement(By.name("company")).click();
-        wd.findElement(By.name("company")).clear();
-        wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
-        wd.findElement(By.name("address")).click();
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-        wd.findElement(By.name("home")).click();
-        wd.findElement(By.name("home")).click();
-        wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys(contactData.getHome());
-        wd.findElement(By.name("mobile")).click();
-        wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
-        wd.findElement(By.name("work")).click();
-        wd.findElement(By.name("work")).clear();
-        wd.findElement(By.name("work")).sendKeys(contactData.getWork());
-        wd.findElement(By.name("fax")).click();
-        wd.findElement(By.name("fax")).clear();
-        wd.findElement(By.name("fax")).sendKeys(contactData.getFax());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(contactData.getEmeil());
-        wd.findElement(By.name("email2")).click();
-        wd.findElement(By.name("email2")).click();
-        wd.findElement(By.name("email2")).click();
-        wd.findElement(By.name("email2")).click();
-        wd.findElement(By.name("email2")).click();
-        wd.findElement(By.name("email2")).clear();
-        wd.findElement(By.name("email2")).sendKeys(contactData.getEmeil2());
-        wd.findElement(By.name("email3")).click();
-        wd.findElement(By.name("email3")).click();
-        wd.findElement(By.name("email3")).click();
-        wd.findElement(By.name("email3")).clear();
-        wd.findElement(By.name("email3")).sendKeys(contactData.getEmeil3());
-        wd.findElement(By.name("homepage")).click();
-        wd.findElement(By.name("homepage")).clear();
-        wd.findElement(By.name("homepage")).sendKeys(contactData.getHomepage());
+        type(By.name("company"),contactData.getCompany());
+        type(By.name("address"),contactData.getAddress());
+        type(By.name("home"),contactData.getHome());
+        type(By.name("mobile"),contactData.getMobile());
+        type(By.name("work"),contactData.getWork());
+        type(By.name("fax"),contactData.getFax());
+        type(By.name("email"),contactData.getEmeil());
+        type(By.name("email2"),contactData.getEmeil2());
+        type(By.name("email3"),contactData.getEmeil3());
+        type(By.name("homepage"),contactData.getHomepage());
+
         wd.findElement(By.name("bday")).click();
         new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBday());
         wd.findElement(By.xpath("//option[@value='5']")).click();
+
         wd.findElement(By.name("bmonth")).click();
         new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBmonth());
         wd.findElement(By.xpath("//option[@value='January']")).click();
-        wd.findElement(By.name("byear")).click();
-        wd.findElement(By.name("byear")).clear();
-        wd.findElement(By.name("byear")).sendKeys(contactData.getByear());
+
+        type(By.name("byear"),contactData.getByear());
+
         wd.findElement(By.name("aday")).click();
         new Select(wd.findElement(By.name("aday"))).selectByVisibleText(contactData.getAday());
         wd.findElement(By.xpath("//div[@id='content']/form/select[3]/option[7]")).click();
+
         wd.findElement(By.name("amonth")).click();
         new Select(wd.findElement(By.name("amonth"))).selectByVisibleText("January");
         wd.findElement(By.xpath("//div[@id='content']/form/select[4]/option[2]")).click();
-        wd.findElement(By.name("ayear")).click();
-        wd.findElement(By.name("ayear")).clear();
-        wd.findElement(By.name("ayear")).sendKeys(contactData.getAyear());
+
+        type(By.name("ayear"),contactData.getAyear());
+
         wd.findElement(By.name("address2")).click();
         wd.findElement(By.xpath("//div[@id='content']/form/label[23]")).click();
+
         wd.findElement(By.name("address2")).click();
         wd.findElement(By.name("address2")).clear();
         wd.findElement(By.name("address2")).sendKeys(contactData.getAddress2());
-        wd.findElement(By.name("phone2")).click();
-        wd.findElement(By.name("phone2")).clear();
-        wd.findElement(By.name("phone2")).sendKeys(contactData.getPhone2());
-        wd.findElement(By.name("notes")).click();
-        wd.findElement(By.name("notes")).clear();
-        wd.findElement(By.name("notes")).sendKeys(contactData.getNotes());
+
+        type(By.name("phone2"),contactData.getPhone2());
+        type(By.name("notes"),contactData.getNotes());
+
     }
 
 
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -14,10 +14,10 @@ public class PointTests {
     }
     @Test
     public void testDistance2 (){
-        Point p1= new Point (5, 10)   ;
-        Point p2= new Point (5, 10)   ;
+        Point p1= new Point (-5, 8.8)   ;
+        Point p2= new Point (-5, -8.8)   ;
 
-        Assert.assertEquals( p2.distance(p1) ,0.1);
+        Assert.assertEquals( p2.distance(p1) ,17.6);
     }
     @Test
     public void testDistance3 (){
@@ -29,15 +29,15 @@ public class PointTests {
     @Test
     public void testDistance4 (){
         Point p1= new Point (0.5, 5.5)   ;
-        Point p2= new Point (5, 10)   ;
+        Point p2= new Point (5, 0.555)   ;
 
-        Assert.assertEquals( p2.distance(p1) ,0.1);
+        Assert.assertEquals( p2.distance(p1) ,6.686032081885339);
     }
     @Test
     public void testDistance5 (){
         Point p1= new Point (5, 10)   ;
-        Point p2= new Point (-5, -10)   ;
+        Point p2= new Point (-5.888, -10)   ;
 
-        Assert.assertEquals( p2.distance(p1) ,22.360679774997898);
+        Assert.assertEquals( p2.distance(p1) ,22.77166098465371);
     }
 }
