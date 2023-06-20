2e07b10608524fa8a31a782c48593c23ff5135cb
EkaterinaMelnikova <katkimo@gmail.com>
2023-06-20 06:40:53 +0300

    Предварительная проверка значения в поле

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/CreateGroupTestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/CreateGroupTestBase.java
@@ -11,7 +11,7 @@ public class CreateGroupTestBase extends TestBase {
 
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("test6", null, null));
+        app.getGroupHelper().fillGroupForm(new GroupData("test6", "test6", "test6"));
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnToGroupPage();
     }
