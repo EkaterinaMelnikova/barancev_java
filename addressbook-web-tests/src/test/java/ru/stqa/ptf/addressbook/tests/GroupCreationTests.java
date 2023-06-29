03ada4793793051fe76973376a4077e8a3c23d64
EkaterinaMelnikova <katkimo@gmail.com>
2023-06-29 07:35:35 +0300

    Сравнение элементов для создания группы л 4.8

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -14,13 +15,25 @@ public class GroupCreationTests extends TestBase {
 
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before =app.getGroupHelper().getGroupList();
+        GroupData group =new GroupData("test6", "test6", "test6");
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("test6", "test6", "test6"));
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after =app.getGroupHelper().getGroupList();
 
         Assert.assertEquals(after.size(),before.size()+1);
+
+
+        int max=0;
+        for (GroupData g: after)  {
+            if (g.getId()> max) {
+                max = g.getId() ;
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
 
     }
 
