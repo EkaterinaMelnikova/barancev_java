db7f2bc325f9bd82b6630a54049f3ee98241ba77
EkaterinaMelnikova <katkimo@gmail.com>
2023-06-29 06:06:22 +0300

    4.7 множества

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/model/GroupData.java
@@ -1,21 +1,23 @@
 package ru.stqa.ptf.addressbook.model;
 
 public class GroupData {
+    private final String id;
     private final String name;
     private final String header;
     private final String footer;
 
     public GroupData(String name, String header, String footer) {
+        this.id = null;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "name='" + name + '\'' +
-                '}';
+    public GroupData(String id, String name, String header, String footer) {
+        this.id = id;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
     }
 
     @Override
@@ -25,14 +27,27 @@ public class GroupData {
 
         GroupData groupData = (GroupData) o;
 
-        if (name != null ? !name.equals(groupData.name) : groupData.name != null) return false;
-        if (header != null ? !header.equals(groupData.header) : groupData.header != null) return false;
-        return footer != null ? footer.equals(groupData.footer) : groupData.footer == null;
+        if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
     }
 
     @Override
     public int hashCode() {
-        return name != null ? name.hashCode() : 0;
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getName() {
--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupModificationTests.java
@@ -4,7 +4,10 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
+
+import static sun.security.pkcs11.wrapper.Functions.getId;
 
 public class GroupModificationTests extends TestBase{
 
@@ -18,11 +21,17 @@ public class GroupModificationTests extends TestBase{
         List<GroupData> before =app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size()-1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test6", "6", "6"));
+        GroupData group =new GroupData(before.get(before.size()-1).getId(),"test6", "6", "6");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after =app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(),before.size());
+
+
+        before.remove(before.size()-1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
 
     }
 }
