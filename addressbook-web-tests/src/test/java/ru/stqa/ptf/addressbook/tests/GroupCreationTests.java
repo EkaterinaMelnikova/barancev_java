05f9389a34e4ffe6042861733afa8ca03d31ad94
EkaterinaMelnikova <katkimo@gmail.com>
2023-06-30 12:23:47 +0300

    4.9 лямбда-выражения в GroupCreation

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -31,7 +32,8 @@ public class GroupCreationTests extends TestBase {
                 max = g.getId() ;
             }
         }
-        group.setId(max);
+
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
 
